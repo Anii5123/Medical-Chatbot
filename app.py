b22a5ec879258772aacbc8ac1358ef41dbd048b5
Anii5123 <anipatil5122003@gmail.com>
2025-08-09 18:40:19 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -113,4 +113,5 @@ def get_response():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=8083)
+    port = int(os.environ.get("PORT", 8083))
+    app.run(debug=True, host="0.0.0.0", port=port)
